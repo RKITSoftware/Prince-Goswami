13dec00d8b8d404dbe01f308f209610871bd70cb
Prince-Goswami <princegoswami.me@gmail.com>
2024-01-29 18:25:32 +0530

    Versoning Completed

---
+++ API Basic Training/Code/Basics of Web Develpoment/Versioning/Controllers/CLProductsV1Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Versioning.Models;
+
+namespace Versioning.Controllers
+{
+    /// <summary>
+    /// Controller for handling version 1 of the demo API.
+    /// </summary>
+    [RoutePrefix("api/v1/demo")]
+    public class CLProductsV1Controller : ApiController
+    {
+        /// <summary>
+        /// Gets a list of products for version 1 of the API.
+        /// </summary>
+        /// <returns>IHttpActionResult with the list of products.</returns>
+        [Route("Products")]
+        [HttpGet]
+        public IHttpActionResult GetProducts()
+        {
+            // Mock data for version 1
+            var products = new List<BLProductV1>
+            {
+                new BLProductV1 { Id = 1, Name = "Product 1" },
+                new BLProductV1 { Id = 2, Name = "Product 2" }
+            };
+
+            return Ok(products);
+        }
+    }
+}
