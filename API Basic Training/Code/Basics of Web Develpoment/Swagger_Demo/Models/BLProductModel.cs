ae39e54e7bf1181d8a3b4ed6c043b39131580657
Prince-Goswami <princegoswami.me@gmail.com>
2024-01-29 18:26:42 +0530

    Swagger demo

---
+++ API Basic Training/Code/Basics of Web Develpoment/Swagger_Demo/Models/BLProductModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Swagger_Demo.Models
+{
+    /// <summary>
+    /// Represents a product entity.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the product.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the product.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
