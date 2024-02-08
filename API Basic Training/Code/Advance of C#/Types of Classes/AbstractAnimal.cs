fdc876205f69e09a3f187e1227178a279bfefa91
Prince-Goswami <princegoswami.me@gmail.com>
2024-02-08 13:09:04 +0530

    Types of Classes

---
+++ API Basic Training/Code/Advance of C#/Types of Classes/AbstractAnimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace TypesOfClass
+{
+    /// <summary>
+    /// This is an abstract class representing a generic animal in the zoo.
+    /// </summary>
+    public abstract class AbstractAnimal
+    {
+        /// <summary>
+        /// Gets the sound the animal makes.
+        /// </summary>
+        /// <returns>The sound of the animal.</returns>
+        public abstract string GetSound();
+
+        /// <summary>
+        /// Display method to show information about the animal.
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine($"Type: {this.GetType().Name}, Sound: {GetSound()}");
+        }
+    }
+}
---
+++ API Basic Training/Code/Advance of C#/Types of Classes/ZooUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesOfClass
+{
+    /// <summary>
+    /// This is a static class representing utility functions for managing the zoo.
+    /// </summary>
+    public static class ZooUtility
+    {
+        /// <summary>
+        /// Static method to feed all animals in the zoo.
+        /// </summary>
+        public static void FeedAllAnimals()
+        {
+            Console.WriteLine("Feeding all animals in the zoo!");
+        }
+    }
+
+}
