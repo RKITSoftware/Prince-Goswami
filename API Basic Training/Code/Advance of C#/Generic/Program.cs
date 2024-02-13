1206e4200b857b840516dd47f597268e76190ac9
Prince-Goswami <princegoswami.me@gmail.com>
2024-02-13 12:38:00 +0530

    generics

---
+++ API Basic Training/Code/Advance of C#/Generic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypesOfClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Creating a ZooContainer for Lions
+            ZooContainer<Lion> lionContainer = new ZooContainer<Lion>();
+            lionContainer.AddAnimal(new Lion());
+            lionContainer.DisplayAnimals();
+
+            // Creating a ZooContainer for Penguins
+            ZooContainer<Penguin> penguinContainer = new ZooContainer<Penguin>();
+            penguinContainer.AddAnimal(new Penguin());
+            penguinContainer.DisplayAnimals();
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ API Basic Training/Code/Advance of C#/Generic/ZooContainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypesOfClass;
+
+namespace TypesOfClass
+{
+    /// <summary>
+    /// This is a generic class representing a ZooContainer that can hold different types of animals.
+    /// </summary>
+    /// <typeparam name="T">The type of animal that the container can hold.</typeparam>
+    public class ZooContainer<T> where T : AbstractAnimal
+    {
+        private List<T> animals;
+
+        public ZooContainer()
+        {
+            animals = new List<T>();
+        }
+
+        /// <summary>
+        /// Adds an animal to the container.
+        /// </summary>
+        /// <param name="animal">The animal to add.</param>
+        public void AddAnimal(T animal)
+        {
+            animals.Add(animal);
+        }
+
+        /// <summary>
+        /// Displays all animals in the container.
+        /// </summary>
+        public void DisplayAnimals()
+        {
+            Console.WriteLine($"Animals in the container:");
+            foreach (var animal in animals)
+            {
+                animal.Display();
+            }
+        }
+    }
+
+}
