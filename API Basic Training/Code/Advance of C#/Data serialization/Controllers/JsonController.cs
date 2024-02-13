b2c4b3a132f3a0269ada470e018a66cec0ac82ca
Prince-Goswami <princegoswami.me@gmail.com>
2024-02-13 12:38:49 +0530

    Data Serialization

---
+++ API Basic Training/Code/Advance of C#/Data serialization/Controllers/JsonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using System.Web.Http.Controllers;
+using Data_serialization.Models;
+using Newtonsoft.Json;
+
+namespace Data_serialization.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    public class PersonController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult JsonToString()
+        {
+            // JSON to String
+            var person = new Person { Name = "Alice", Age = 28 };
+            string jsonString = JsonConvert.SerializeObject(person);
+            return Ok(jsonString);
+        }
+
+        [HttpPost]
+        public IHttpActionResult StringToJson([FromBody] string jsonInput)
+        {
+            // String to JSON
+            var deserializedPerson = JsonConvert.DeserializeObject<Person>(jsonInput);
+            return Ok(deserializedPerson);
+        }
+    }
+
+}
---
+++ API Basic Training/Code/Advance of C#/Data serialization/Controllers/XMLController.cs
@@ -0,0 +1,40 @@
+﻿
+using System.IO;
+using System.Web.Http;
+using System.Xml.Serialization;
+using Data_serialization.Models;
+
+namespace Data_serialization.Controllers
+{
+
+    [RoutePrefix("api/[controller]")]
+    public class XMLController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult XmlToString()
+        {
+            // XML to String
+            var person = new Person { Name = "Charlie", Age = 22 };
+            var serializer = new XmlSerializer(typeof(Person));
+            using (var stringWriter = new StringWriter())
+            {
+                serializer.Serialize(stringWriter, person);
+                string xmlString = stringWriter.ToString();
+                return Ok(xmlString);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult StringToXml([FromBody] string xmlInput)
+        {
+            // String to XML
+            var serializer = new XmlSerializer(typeof(Person));
+            using (var stringReader = new StringReader(xmlInput))
+            {
+                var deserializedPerson = (Person)serializer.Deserialize(stringReader);
+                return Ok(deserializedPerson);
+            }
+        }
+    }
+
+}
